f04da65b8a0c724e00c361c2ae259354558f412e
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-04-15 01:34:15 +0900

    solve 7662 - 이중 우선순위 큐
    gold 4
    우선순위 큐는 반대쪽 원소의 접근이 어려우므로 사용하지 않고, 자동으로 정렬되는 셋의 특성을 이용하여 해결하였다.
    직접 구현하면 엄청 어려울 듯.

---
+++ Algorithm/Algorithm/7662 - 이중 우선순위 큐.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+std::multiset<int, std::greater<int>> set;
+
+int testcase, datanum, inputnum;
+std::string inputstr;
+
+int main()
+{
+	std::cin.tie(NULL);
+	std::ios::sync_with_stdio(false);
+
+	std::cin >> testcase;
+	for (int i = 0; i < testcase; ++i) {
+		std::cin >> datanum;
+		for (int j = 0; j < datanum; ++j) {
+			std::cin >> inputstr >> inputnum;
+			if (inputstr == "I") {
+				set.insert(inputnum);
+			}
+			else if (inputstr == "D") {
+				if (set.empty()) {
+
+				}
+				else if (inputnum == 1) {
+					std::cout << *set.begin() << "삭제\n";
+					set.erase(set.begin());
+				}
+				else if (inputnum == -1) {
+					std::cout << *(--set.end()) << "삭제\n";
+					set.erase(--set.end());
+				}
+			}
+		}
+		if (set.empty()) {
+			std::cout << "EMPTY\n";
+		}
+		else {
+			std::cout << *set.begin() <<  " " << *(--set.end()) << "\n";
+		}
+		set.erase(set.begin(), set.end());
+	}
+}
