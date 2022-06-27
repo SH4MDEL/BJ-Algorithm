1626774e56349b4dbe8f9beca579785703079aa9
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-06-28 00:33:35 +0900

    solve 2294 - 동전 2
    gold 5
    dp 문제. 주어진 동전의 경우 한번만에 그 동전의 가치를 만들어 낼 수 있기 때문에, k원을 만드는 최소 개수가 n이라고 하면 k - 주어진 동전 을 만드는 최소 개수는 n - 1이라는 점에 착안해 탑다운 방식으로 해결

--- Algorithm/Algorithm/2294 - 동전 2.cpp
+++ Algorithm/Algorithm/2294 - 동전 2.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 #define endl "\n"
+#define inf 987654321
 
 int N, K;
 int data[100];
-int cache[101][10001];
+int cache[10001];
 
-int solve(int n, int k)
+int solve(int k)
 {
-	if (cache[n][k] != -1) {
-		return cache[n][k];
+	if (k < 0) {
+		return inf;
 	}
-	for (int i = data[n]; i < k; i + data[n]) {
-		cache[n][i] = 1 + solve(n + 1, k - data[n]);
+	if (cache[k] != -1) {
+		return cache[k];
 	}
+	int min = inf;
+	for (int i = 0; i < N; ++i) {
+		if (min > solve(k - data[i]) + 1) {
+			min = solve(k - data[i]) + 1;
+		}
+	}
+	return cache[k] = min;
+
 }
 
 int main()
 {
 	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
 
+	for (int i = 0; i < 10001; ++i) {
+		cache[i] = -1;
+	}
+
 	std::cin >> N >> K;
 	for (int i = 0; i < N; ++i) {
 		std::cin >> data[i];
+		cache[data[i]] = 1;
 	}
-	std::cout << solve(0, K);
+	std::cout << solve(K);
 }
