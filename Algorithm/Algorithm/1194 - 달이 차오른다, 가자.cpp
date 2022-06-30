0bfb10dd417636bad6daa03348c80e6fb5ac3537
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-07-01 00:26:54 +0900

    solving 1194 - 달이 차오른다, 가자
    gold 1
    너비우선탐색으로 바꾸자

--- Algorithm/Algorithm/1194 - 달이 차오른다, 가자.cpp
+++ Algorithm/Algorithm/1194 - 달이 차오른다, 가자.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
+#include <map>
 #define endl "\n"
 #define inf 987654321
 using namespace std;
 
-char map[52][52];
-bool visited[52][52][((1 << 6) + 1)];
+char cmap[52][52];
 int n, m;
+
+map<pair<int, int>, int> visited_map;
 
 int depthcount = inf;
 
-bool Search(int x, int y, int key, int depth)
+void Search(int x, int y, int key, int depth)
 {
+	if (cmap[y][x] >= 'a' && cmap[y][x] <= 'f') {
+		key |= (1 << (int)(cmap[y][x] - 'a'));
+
+	}
+
+	if ((visited_map.find(make_pair(y * (n + 1) + x, key))) != visited_map.end()) {
+		if (visited_map[make_pair(y * (n + 1) + x, key)] < depth) {
+			return;
+		}
+	}
+	visited_map[make_pair(y * (n + 1) + x, key)] = depth;
+
 	if (depth > depthcount) {
-		return false;
+		return;
 	}
-	if (visited[y][x][key]) {
-		return false;
-	}
-	visited[y][x][key] = true;
-	if (map[y][x] == '#') {
-		return false;
-	}
-	if (map[y][x] >= 'a' && map[y][x] <= 'f') {
-		key |= (1 << (int)(map[y][x] - 'a' + 1));
-	}
-	if (map[y][x] >= 'A' && map[y][x] <= 'F') {
-		if (key & (1 << (int)(map[y][x] - 'A' + 1))) {
+	if (cmap[y][x] >= 'A' && cmap[y][x] <= 'F') {
+		if (key & (1 << (int)(cmap[y][x] - 'A'))) {
 		}
 		else {
-			return false;
+			return;
 		}
 	}
-	if (map[y][x] == '1') {
+	if (cmap[y][x] == '#') {
+		return;
+	}
+
+	if (cmap[y][x] == '1') {
 		if (depthcount > depth) {
 			depthcount = depth;
 		}
-		return true;
+		return;
 	}
+
+
 	Search(x - 1, y, key, depth + 1);
 	Search(x + 1, y, key, depth + 1);
 	Search(x, y - 1, key, depth + 1);
@@ -51,17 +61,14 @@ int main()
 
 	for (int i = 0; i < 52; ++i) {
 		for (int j = 0; j < 52; ++j) {
-			map[i][j] = '#';
-			for (int k = 0; k < ((1 << 6) + 1); ++k) {
-				visited[i][j][k] = false;
-			}
+			cmap[i][j] = '#';
 		}
 	}
 	cin >> n >> m;
 	for (int i = 1; i <= n; ++i) {
 		for (int j = 1; j <= m; ++j) {
-			cin >> map[i][j];
-			if (map[i][j] == '0') {
+			cin >> cmap[i][j];
+			if (cmap[i][j] == '0') {
 				starty = i, startx = j;
 			}
 		}
