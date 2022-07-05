77b38204b5cbb8931826f8fb1fbb711375b527fb
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-07-05 20:01:53 +0900

    solving 1194 - 달이 차오른다, 가자.
    gold 1

--- Algorithm/Algorithm/1194 - 달이 차오른다, 가자.cpp
+++ Algorithm/Algorithm/1194 - 달이 차오른다, 가자.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <queue>
 #define endl "\n"
 #define inf 987654321
 using namespace std;
@@ -8,6 +9,7 @@ char cmap[52][52];
 int n, m;
 
 map<pair<int, int>, int> visited_map;
+queue<pair<int, int>> q;
 
 int depthcount = inf;
 
@@ -53,6 +55,163 @@ void Search(int x, int y, int key, int depth)
 	Search(x, y + 1, key, depth + 1);
 }
 
+void bfs(int x, int y, int key) 
+{
+	q.push(make_pair(y * (n + 1) + x, key));
+	visited_map[make_pair(y * (n + 1) + x, key)] = 1;
+
+	[&]() {
+		while (!q.empty()) {
+			int qsize = q.size();
+
+			for (int i = 0; i < qsize; ++i) {
+				pair<int, int> current = q.front();
+				cout << (current.first) / (n + 1) << ", " << (current.first) % (n + 2) << endl;
+				cout << (current.first + (n + 1)) / (n + 1) << ", " << (current.first + (n + 1)) % (n + 1) << endl;
+				cout << (current.first - (n + 1)) / (n + 1) << ", " << (current.first - (n + 1)) % (n + 1) << endl;
+				cout << (current.first + 1) / (n + 1) << ", " << (current.first + 1) % (n + 1) << endl;
+				cout << (current.first - 1) / (n + 1) << ", " << (current.first - 1) % (n + 1) << endl << endl;
+				q.pop();
+
+				//if ((visited_map.find(make_pair(current.first + (n + 1), current.second))) != visited_map.end()) {
+				if (cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] >= 'a' &&
+					cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] <= 'z') {
+					int newkey = current.second | (1 << (int)(cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] - 'a'));
+
+					if (visited_map.find(make_pair(current.first + (n + 1), newkey)) == visited_map.end()) {
+						q.push(make_pair(current.first + (n + 1), newkey));
+						visited_map[make_pair(current.first + (n + 1), newkey)] += 1;
+					}
+				}
+				if (cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] >= 'A' &&
+					cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] <= 'Z') {
+					if (current.second & (1 << (int)(cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] - 'A'))) {
+
+						if (visited_map.find(make_pair(current.first + (n + 1), current.second)) == visited_map.end()) {
+							q.push(make_pair(current.first + (n + 1), current.second));
+							visited_map[make_pair(current.first + (n + 1), current.second)] = visited_map[make_pair(current.first, current.second)] + 1;
+						}
+					}
+				}
+				if (cmap[(current.first + (n + 1)) / (n + 1)][(current.first + (n + 1)) % (n + 1)] == '.') {
+
+					if (visited_map.find(make_pair(current.first + (n + 1), current.second)) == visited_map.end()) {
+						q.push(make_pair(current.first + (n + 1), current.second));
+						visited_map[make_pair(current.first + (n + 1), current.second)] += 1;
+					}
+				}
+				if ((current.first + (n + 1)) / (n + 1) == n && (current.first + (n + 1)) % (n + 1) == m) {
+					depthcount = visited_map[make_pair(current.first, current.second)] + 1;
+					return;
+				}
+
+
+				if (cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] >= 'a' &&
+					cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] <= 'z') {
+					int newkey = current.second | (1 << (int)(cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] - 'a'));
+
+					if (visited_map.find(make_pair(current.first - (n + 1), newkey)) == visited_map.end()) {
+						q.push(make_pair(current.first - (n + 1), newkey));
+						visited_map[make_pair(current.first - (n + 1), newkey)] += 1;
+					}
+				}
+				if (cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] >= 'A' &&
+					cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] <= 'Z') {
+					if (current.second & (1 << (int)(cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] - 'A'))) {
+
+						if (visited_map.find(make_pair(current.first - (n + 1), current.second)) == visited_map.end()) {
+							q.push(make_pair(current.first - (n + 1), current.second));
+							visited_map[make_pair(current.first - (n + 1), current.second)] = visited_map[make_pair(current.first, current.second)] + 1;
+						}
+					}
+				}
+				if (cmap[(current.first - (n + 1)) / (n + 1)][(current.first - (n + 1)) % (n + 1)] == '.') {
+
+					if (visited_map.find(make_pair(current.first - (n + 1), current.second)) == visited_map.end()) {
+						q.push(make_pair(current.first + (n + 1), current.second));
+						visited_map[make_pair(current.first + (n + 1), current.second)] += 1;
+					}
+				}
+				if ((current.first - (n + 1)) / (n + 1) == n && (current.first - (n + 1)) % (n + 1) == m) {
+					depthcount = visited_map[make_pair(current.first, current.second)] + 1;
+					return;
+				}
+
+
+				if (cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] >= 'a' &&
+					cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] <= 'z') {
+					int newkey = current.second | (1 << (int)(cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] - 'a'));
+
+					if (visited_map.find(make_pair(current.first + 1, newkey)) == visited_map.end()) {
+						q.push(make_pair(current.first + 1, newkey));
+						visited_map[make_pair(current.first + 1, newkey)] += 1;
+					}
+				}
+				if (cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] >= 'A' &&
+					cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] <= 'Z') {
+					if (current.second & (1 << (int)(cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] - 'A'))) {
+
+						if (visited_map.find(make_pair(current.first + 1, current.second)) == visited_map.end()) {
+							q.push(make_pair(current.first + 1, current.second));
+							visited_map[make_pair(current.first + 1, current.second)] = visited_map[make_pair(current.first, current.second)] + 1;
+						}
+					}
+				}
+				if (cmap[(current.first + 1) / (n + 1)][(current.first + 1) % (n + 1)] == '.') {
+
+					if (visited_map.find(make_pair(current.first + 1, current.second)) == visited_map.end()) {
+						q.push(make_pair(current.first + 1, current.second));
+						visited_map[make_pair(current.first + 1, current.second)] += 1;
+					}
+				}
+				if ((current.first + 1) / (n + 1) == n && (current.first + 1) % (n + 1) == m) {
+					depthcount = visited_map[make_pair(current.first, current.second)] + 1;
+					return;
+				}
+
+
+				if (cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] >= 'a' &&
+					cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] <= 'z') {
+					int newkey = current.second | (1 << (int)(cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] - 'a'));
+
+					if (visited_map.find(make_pair(current.first - 1, newkey)) == visited_map.end()) {
+						q.push(make_pair(current.first - 1, newkey));
+						visited_map[make_pair(current.first - 1, newkey)] += 1;
+					}
+				}
+				if (cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] >= 'A' &&
+					cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] <= 'Z') {
+					if (current.second & (1 << (int)(cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] - 'A'))) {
+
+						if (visited_map.find(make_pair(current.first - 1, current.second)) == visited_map.end()) {
+							q.push(make_pair(current.first - 1, current.second));
+							visited_map[make_pair(current.first - 1, current.second)] = visited_map[make_pair(current.first, current.second)] + 1;
+						}
+					}
+				}
+				if (cmap[(current.first - 1) / (n + 1)][(current.first - 1) % (n + 1)] == '.') {
+
+					if (visited_map.find(make_pair(current.first - 1, current.second)) == visited_map.end()) {
+						q.push(make_pair(current.first - 1, current.second));
+						visited_map[make_pair(current.first - 1, current.second)] += 1;
+					}
+				}
+				if ((current.first - 1) / (n + 1) == n && (current.first - 1) % (n + 1) == m) {
+					depthcount = visited_map[make_pair(current.first, current.second)] + 1;
+					return;
+				}
+
+				//}
+			}
+		}
+	}();
+}
+
+void bfsall(int startx, int starty)
+{
+
+}
+
 int main()
 {
 	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
@@ -73,7 +232,8 @@ int main()
 			}
 		}
 	}
-	Search(startx, starty, 0, 0);
+	cout << "start : " << starty << ", " << startx << endl;
+	bfs(startx, starty, 0);
 	if (depthcount == inf) {
 		cout << "-1" << endl;
 	}
