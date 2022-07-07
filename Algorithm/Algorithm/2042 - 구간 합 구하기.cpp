42adc1c774593d6ae525ea9f9925ebd3f3c1c708
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-07-08 00:13:36 +0900

    solve 2042 - 구간 합 구하기
    gold 1
    공부한 코드: https://www.acmicpc.net/blog/view/9
    세그먼트 트리에 관해 공부했다.
    세그먼트 트리는 트리 형태로 자료를 관리하며, 원래 값은 트리의 노드에만 저장되어 있다. (따라서 많은 메모리를 사용한다. 원래 값의 4배 정도)
    초기화에서는 현재 노드가 루트가 아닐 경우에 재귀호출하며 두 자식 노드의 합을 노드에 저장한다.
    특정 구간의 합을 구하고 싶을 경우 구간이 포함된 노드를 타고 내려가며 정확히 일치하는 구간을 찾는다.
    값의 갱신은 관련된 노드의 루트까지 타고 내려하며 하나씩 갱신해 준다.
    비슷한 문제를 계속 풀어보며 익숙해지자.

--- Algorithm/Algorithm/2042 - 구간 합 구하기.cpp
+++ Algorithm/Algorithm/2042 - 구간 합 구하기.cpp
@@ -4,14 +4,72 @@
 #define inf 987654321
 using namespace std;
 
-std::vector<long long> data;
-std::vector<long long> tree;
-int N, M, K, num, left, right;
+int n, m, k;
 
+long long init(vector<long long>& arr, vector<long long>& tree, int node, int start, int end)
+{
+	// 리프 노드 도착
+	if (start == end) {
+		return tree[node] = arr[start];
+	}
+
+	// 현재 노드의 값은 자식 노드 두 개의 합
+	int mid = (start + end) / 2;
+	return tree[node] = init(arr, tree, node * 2, start, mid) + init(arr, tree, node * 2 + 1, mid + 1, end);
+}
+
+long long find_sum(vector<long long>& tree, int node, int start, int end, int left, int right)
+{
+	// case 1: [start, end] 앞 뒤에 [left, right]가 없다.
+	// 즉 교집합이 없기 때문에 더 탐색할 필요가 없다.
+	if (left > end || right < start) return 0;
+
+	// case 2: [left, right]가 [start, end]를 포함
+	if (left <= start && end <= right) {
+		return tree[node];
+	}
+	// case 3: [start, end]가 [left, right]를 포함하거나
+	// case 4: [left, right]와 [start, end]가 겹쳐져 있는 경우
+	int mid = (start + end) / 2;
+	return find_sum(tree, node * 2, start, mid, left, right) + find_sum(tree, node * 2 + 1, mid + 1, end, left, right);
+}
+
+void update(vector<long long>& tree, int node, int start, int end, int index, long long diff)
+{
+	// diff는 업데이트된 수와 기존 수의 차이이다.
+	if (index < start || index > end) return;
+	tree[node] += diff;
+
+	//루트노드가 아니라면
+	if (start != end) {
+		int mid = (start + end) / 2;
+		update(tree, node * 2, start, mid, index, diff);
+		update(tree, node * 2 + 1, mid + 1, end, index, diff);
+	}
+}
 
 int main()
 {
-	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
+	cin >> n >> m >> k;
 
+	vector<long long> arr(n);
+	vector<long long> segtree(n * 4);
 
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+	init(arr, segtree, 1, 0, n - 1);
+
+	int query, a, b;
+	for (int i = 0; i < m + k; ++i) {
+		cin >> query >> a >> b;
+		if (query == 1) {
+			long long diff = (long long)b - arr[a - 1];
+			arr[a - 1] = b;
+			update(segtree, 1, 0, n - 1, a - 1, diff);
+		}
+		if (query == 2) {
+			cout << find_sum(segtree, 1, 0, n - 1, a - 1, b - 1) << endl;
+		}
+	}
 }
--- Algorithm/Algorithm/test.cpp
+++ Algorithm/Algorithm/test.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
-#include <string>
-#include <map>
+#include <math.h>
 #define inf 987654321 
 #define endl "\n"
 using namespace std;
 
-int n, m;
-
-map<int, string> intmap;
-map<string, int> stringmap;
+int n, sum;
 
 int main()
 {
 	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
 
-	cin >> n >> m;
-	string inputstring;
-	for (int i = 1; i <= n; ++i) {
-		cin >> inputstring;
-		stringmap[inputstring] = i;
-		intmap[i] = inputstring;
+	cin >> n;
+	while (n != 0) {
+		sum += pow(n % 10, 5);
+		n /= 10;
 	}
-	for (int i = 0; i < m; ++i) {
-		cin >> inputstring;
-		if (inputstring[0] >= 48 && inputstring[0] <= 57) {
-			cout << intmap[stoi(inputstring)] << endl;
-		}
-		else {
-			cout << stringmap[inputstring] << endl;
-		}
-	}
+	cout << sum << endl;
 }
