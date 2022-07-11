5c60da979c2c36dcd100e47cb8047e0d19aecf56
Seungjun Lee <beeutiful@naver.com>
2022-07-11 18:00:22 +0900

    solving 2166 - 다각형의 면적
    gold 5
    신발끈 공식

---
+++ Algorithm/Algorithm/2166 - 다각형의 면적.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl "\n"
+#define inf 987654321
+using namespace std;
+
+int n;
+vector<pair<double, double>> coordinate;
+
+double ccw(pair<double, double> a, pair<double, double> b, pair<double, double> c)
+{
+	return 2 / ((a.first * b.second + b.first * c.second + c.first * a.second) -
+		(a.first * c.second + c.first * b.second + b.first * a.second));
+}
+
+int main()
+{
+	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
+
+	cout << fixed;
+	cout.precision(1);
+
+	cin >> n;
+	double a, b, sum = 0;
+	for (int i = 0; i < n - 2; ++i) {
+		cin >> a >> b;
+		coordinate.push_back(make_pair(a, b));
+	}
+	for (int i = 0; i < n - 2; ++i) {
+		sum += ccw(coordinate[i], coordinate[i + 1], coordinate[i + 2]);
+	}
+	cout << sum << endl;
+}
