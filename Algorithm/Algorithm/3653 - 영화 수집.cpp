72a82dc48a1162eb3965935c370322ad94af7f4c
Seungjun Lee <beeutiful@naver.com>
2022-07-12 17:11:59 +0900

    solve 3653 - 영화 수집
    platinum 4
    영화를 새롭게 위로 옮기는 과정에서 최대 m번의 이동이 발생하므로, 애초부터 처음 영화의 수까지 합쳐 n + m 루트 사이즈의 세그먼트 트리를 확보한다.
    영화를 옮길 때는 n + i번째에 삽입한다.
    (vector.resize(n + m)을 vector.resize(n * m)으로 오타내어 bad_alloc 런타임 에러가 발생했다. n, m은 최대 10만..)

---
+++ Algorithm/Algorithm/3653 - 영화 수집.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl "\n"
+#define inf 987654321
+using namespace std;
+
+int t, n, m;
+
+long long init(vector<long long>& arr, vector<long long>& tree, int node, int start, int end)
+{
+	if (start == end) {
+		return tree[node] = arr[start];
+	}
+	int mid = (start + end) / 2;
+	return tree[node] = init(arr, tree, node * 2, start, mid) + init(arr, tree, node * 2 + 1, mid + 1, end);
+}
+
+long long find(vector<long long>& tree, int node, int start, int end, int left, int right)
+{
+	if (left > end || right < start) return 0;
+
+	if (left <= start && right >= end) {
+		return tree[node];
+	}
+	int mid = (start + end) / 2;
+	return find(tree, node * 2, start, mid, left, right) + find(tree, node * 2 + 1, mid + 1, end, left, right);
+}
+
+void update(vector<long long>& tree, int node, int start, int end, int index, long long diff)
+{
+	if (index < start || index > end) return;
+
+	tree[node] += diff;
+	
+	if (start != end) {
+		int mid = (start + end) / 2;
+		update(tree, node * 2, start, mid, index, diff);
+		update(tree, node * 2 + 1, mid + 1, end, index, diff);
+	}
+}
+
+
+
+int main()
+{
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		cin >> n >> m;
+		vector<long long> answer;
+		answer.reserve(m);
+		vector <long long> arr(n, 1);
+		arr.resize(n * m);
+		vector <long long> segtree((n + m) * 4);
+		init(arr, segtree, 1, 0, n + m - 1);
+
+		for (int j = n - 1; auto& elm : arr) {
+			elm = (long long)j;
+			--j;
+		}
+
+		int query;
+		for (int j = 0; j < m; ++j) {
+			cin >> query;
+			answer.push_back(find(segtree, 1, 0, n + m - 1, arr[query - 1], n + m - 1) - 1);
+			update(segtree, 1, 0, n + m - 1, arr[query - 1], -1);
+			update(segtree, 1, 0, n + m - 1, n + j, 1);
+
+			arr[query - 1] = n + j;
+		}
+
+		for (const auto& elm : answer) {
+			cout << elm << " ";
+		}
+		cout << endl;
+	}
+}
