c5d52ac7922376f570b286b254cc4ec541a77c29
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-07-15 20:51:29 +0900

    solve 1916 - 최소비용 구하기
    gold 5
    다익스트라 문제.
    1753 - 최단경로에 비해 빡빡한 tc가 들어오는 건지 시간제한이 절반이라 그런지 시간초과가 자주 난다.
    더해야하는 cost가 이미 지금의 최소 거리보다도 적은 경우 루프를 생략하는 방법으로 시간 초과 해결.
    근데 왜 난이도는 더 낮은지..

--- Algorithm/Algorithm/1916 - 최소비용 구하기.cpp
+++ Algorithm/Algorithm/1916 - 최소비용 구하기.cpp
@@ -19,9 +19,11 @@ void Search(int start)
 	pq.push(make_pair(0, start));
 
 	while (!pq.empty()) {
-		int current = pq.top().first;
-		int cost = pq.top().second;
+		int cost = pq.top().first;
+		int current = pq.top().second;
 		pq.pop();
+
+		if (dist[current] < cost) continue;
 
 		for (int i = 0; i < vertex[current].size(); ++i) {
 			int sumcost = vertex[current].at(i).first;
@@ -31,6 +33,7 @@ void Search(int start)
 				dist[next] = cost + sumcost;
 				pq.push(make_pair(dist[next], next));
 			}
+
 		}
 	}
 }
