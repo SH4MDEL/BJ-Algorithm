2532a6c2271ceda2a683a23c675b39eb61d016f8
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-07-16 14:34:26 +0900

    solve 1238 - 파티
    gold 3
    다익스트라를 여러번 돌리며 각 점에서 x까지의 거리를 구하고, x에서 다익스트라를 한 번 더 돌려 x에서 모든 점까지의 거리를 구한다.
    다익스트라를 두번만 돌려 풀 수 있는 방법이 있는 듯.

---
+++ Algorithm/Algorithm/1238 - 파티.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define endl "\n"
+#define inf 987'654'321
+using namespace std;
+
+int n, m, x;
+int s, e, t;
+vector<pair<int, int>> vertex[1005];
+int dist[1005];
+int distsum[1005];
+
+void Djikstra(int start, bool isReturn)
+{
+	for (int i = 0; i < 1005; ++i) {
+		dist[i] = inf;
+	}
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	dist[start] = 0;
+	pq.push(make_pair(0, start));
+
+	while (!pq.empty()) {
+		int cost = pq.top().first;
+		int current = pq.top().second;
+		pq.pop();
+
+		if (dist[current] < cost) continue;
+
+		for (int i = 0; i < vertex[current].size(); ++i) {
+			int curcost = vertex[current][i].first;
+			int next = vertex[current][i].second;
+
+			if (dist[next] > cost + curcost) {
+				dist[next] = cost + curcost;
+				pq.push(make_pair(dist[next], next));
+			}
+
+			//if (next == x && isReturn) {
+			//	return;
+			//}
+		}
+	}
+}
+
+int main()
+{
+	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
+
+	cin >> n >> m >> x;
+
+	for (int i = 0; i < m; ++i) {
+		cin >> s >> e >> t;
+		vertex[s].push_back(make_pair(t, e));
+	}
+	Djikstra(x, false);
+	for (int i = 1; i <= n; ++i) {
+		distsum[i] += dist[i];
+	}
+	int maxdist = -1;
+	for (int i = 1; i <= n; ++i) {
+		Djikstra(i, true);
+		distsum[i] += dist[x];
+		if (maxdist < distsum[i]) {
+			maxdist = distsum[i];
+		}
+	}
+	cout << maxdist << endl;
+}
