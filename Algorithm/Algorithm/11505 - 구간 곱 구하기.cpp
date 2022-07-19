cb344a7f3a39d4936fadac466d1b10def96ce153
Seungjun Lee <beeutiful@naver.com>
2022-07-19 20:31:34 +0900

    solve 11505 - 구간 곱 구하기
    gold 1
    bottom-up, top-down 중 어느 구현이 더 알맞을지 먼저 생각해 보도록 하자.

---
+++ Algorithm/Algorithm/11505 - 구간 곱 구하기.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#define endl "\n"
+#define inf 987654321
+#define mod 1'000'000'007
+using namespace std;
+
+long long init(vector<long long>& arr, vector<long long>& tree, int node, int start, int end)
+{
+	if (start == end) return tree[node] = arr[start];
+
+	int mid = (start + end) / 2;
+	return tree[node] = ((init(arr, tree, node * 2, start, mid) % mod) * (init(arr, tree, node * 2 + 1, mid + 1, end) % mod)) % mod;
+}
+
+long long update(vector<long long>& tree, int node, int start, int end, int index, long long diff)
+{
+	if (start > index || index > end) return tree[node];
+
+	if (start == end) return tree[node] = diff;
+
+	int mid = (start + end) / 2;
+	return tree[node] = ((update(tree, node * 2, start, mid, index, diff) % mod) 
+		* (update(tree, node * 2 + 1, mid + 1, end, index, diff) % mod)) % mod;
+}
+
+long long find(vector<long long>& tree, int node, int start, int end, int left, int right)
+{
+	if (left > end || right < start) return 1;
+
+	if (left <= start && end <= right) return tree[node];
+
+	int mid = (start + end) / 2;
+	return ((find(tree, node * 2, start, mid, left, right) % mod) * (find(tree, node * 2 + 1, mid + 1, end, left, right) % mod)) % mod;
+}
+
+int main()
+{
+	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
+
+	int n, m, k;
+	cin >> n >> m >> k;
+	vector<long long> arr(n);
+	vector<long long> segtree(n * 4);
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+	init(arr, segtree, 1, 0, n - 1);
+
+	int query, a, b;
+	for (int i = 0; i < m + k; ++i) {
+		cin >> query >> a >> b;
+		if (query == 1) {
+			arr[a - 1] = (long long)b;
+			update(segtree, 1, 0, n - 1, a - 1, (long long)b);
+		}
+		if (query == 2) {
+			cout << find(segtree, 1, 0, n - 1, a - 1, b - 1) << endl;
+		}
+	}
+}
