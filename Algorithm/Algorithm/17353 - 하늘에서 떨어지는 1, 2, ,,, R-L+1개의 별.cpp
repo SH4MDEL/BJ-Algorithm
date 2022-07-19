4c343edff116af68e79e498b257175d3a40d1d19
Seungjun Lee <beeutiful@naver.com>
2022-07-19 21:37:58 +0900

    solving 17353
    platinum 2
    어렵다..

--- Algorithm/Algorithm/17353 - 하늘에서 떨어지는 1, 2, ,,, R-L+1개의 별.cpp
+++ Algorithm/Algorithm/17353 - 하늘에서 떨어지는 1, 2, ,,, R-L+1개의 별.cpp
@@ -15,22 +15,88 @@ long long init(vector<long long>& arr, vector<long long>& tree, int node, int st
 void update_lazy(vector<long long>& tree, vector<long long>& lazy, int node, int start, int end)
 {
 	if (lazy[node] != 0) {
-		tree[node] += lazy[node];
+		tree[node] += lazy[node] * (end - start + 1);
 		if (start != end) {
+			int mid = (start + end) / 2;
+
 			lazy[node * 2] += lazy[node];
 			lazy[node * 2 + 1] += lazy[node];
 		}
 		lazy[node] = 0;
-		return;
 	}
 }
 
-void update_range(vector<long long>& tree, vector<long long>& lazy, int node, int start, int end, int left, int right, long long diff_start)
+void update_range(vector<long long>& tree, vector<long long>& lazy, int node, int start, int end, int left, int right, long long diff)
 {
 	update_lazy(tree, lazy, node, start, end);
 	if (left > end || right < start) return;
 
+	int mid = (start + end) / 2;
+
 	if (left <= start && end <= right) {
-		tree[node] += (end - start + 1) * (end - start + 2 * diff_start) / 2;
+		tree[node] += (end - start + 1) * diff;
+		if (start != end) {
+			lazy[node * 2] += diff;
+			lazy[node * 2 + 1] += diff;
+		}
+		return;
+	}
+
+	update_range(tree, lazy, node * 2, start, mid, left, right, diff);
+	update_range(tree, lazy, node * 2 + 1, mid + 1, end, left, right, diff);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+long long find(vector<long long>& tree, vector<long long>& lazy, int node, int start, int end, int left, int right)
+{
+	update_lazy(tree, lazy, node, start, end);
+	if (left > end || right < start) return 0;
+
+	if (left <= start && end <= right) return tree[node];
+
+	int mid = (start + end) / 2;
+	return find(tree, lazy, node * 2, start, mid, left, right) + find(tree, lazy, node * 2 + 1, mid + 1, end, left, right);
+}
+
+void print_all(vector<long long>& tree, int node, int start, int end)
+{
+	if (start == end) {
+		cout << tree[node] << " ";
+		return;
+	}
+	int mid = (start + end) / 2;
+	print_all(tree, node * 2, start, mid);
+	print_all(tree, node * 2 + 1, mid + 1, end);
+}
+
+int main()
+{
+	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n;
+	vector<long long> arr(n);
+	vector<long long> segtree(n * 4);
+	vector<long long> lazytree(n * 4);
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+	init(arr, segtree, 1, 0, n - 1);
+	cin >> m;
+
+	int query, a, b;
+	for (int i = 0; i < m; ++i) {
+		cin >> query;
+		if (query == 1) {
+			cin >> a >> b;
+			update_range(segtree, lazytree, 1, 0, n - 1, a - 1, b - 1, 1);
+			update_range(segtree, lazytree, 1, 0, n - 1, b, b, -(b - a + 1));
+			print_all(segtree, 1, 0, n - 1);
+			cout << endl;
+		}
+		if (query == 2) {
+			cin >> a;
+			cout << find(segtree, lazytree, 1, 0, n - 1, a - 1, a - 1) << endl;
+		}
 	}
 }
