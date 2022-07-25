9a4050bd2b2e345b04e07fd1e73394ce8de3c2c6
Seungjun Lee <beeutiful@naver.com>
2022-07-25 17:48:37 +0900

    solve 2517 - 달리기
    platinum 4
    실력이 좋은 선수부터 정렬하여 인덱스 순으로 나열된 segtree에 삽입한다. segtree는 구간 합 트리이며, 리프노드는 1을 저장하고 있다. 원소 하나를 삽입하고 나서 자기 왼쪽에 있는, 즉 자기보다 인덱스가 작은 리프노드부터 자신까지의 합을 저장하여 answer 배열에 저장한다. 이것이 이 원소의 가능한 최대 순위이다.

--- Algorithm/Algorithm/2517 - 달리기.cpp
+++ Algorithm/Algorithm/2517 - 달리기.cpp
@@ -5,18 +5,35 @@
 #define inf 987654321
 using namespace std;
 
-void update(pair<int, int> input, vector<pair<int, int>>& tree, int node, int start, int end)
+int update(pair<int, int> input, vector<int>& tree, int node, int start, int end)
 {
-	if (input.first < start || input.first > end) return;
+	if (input.first < start || input.first > end) return tree[node];
 
-	if (start == end) tree[node] = 
+	if (start == end) return tree[node] = 1;
+
+	int mid = (start + end) / 2;
+	update(input, tree, node * 2, start, mid);
+	update(input, tree, node * 2 + 1, mid + 1, end);
+	return tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+int find(vector<int>& tree, int node, int start, int end, int left, int right)
+{
+	if (left > end || right < start) return 0;
+	if (left <= start && end <= right) return tree[node];
+
+	int mid = (start + end) / 2;
+	return find(tree, node * 2, start, mid, left, right) + find(tree, node * 2 + 1, mid + 1, end, left, right);
 }
 
 int main()
 {
+	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
 	int n;
+	cin >> n;
 	vector<pair<int, int>> arr(n);
-	vector<pair<int, int>> segtree(n * 4);
+	vector<int> segtree(n * 4);
+	vector<int> answer(n);
 	for (int i = 0; i < n; ++i) {
 		arr[i].first = i;
 		cin >> arr[i].second;
@@ -27,5 +44,10 @@ int main()
 	for (int i = 0; i < n; ++i) {
 		arr[i].second = i;
 	}
+	for (int i = 0; i < n; ++i) {
+		update(arr[i], segtree, 1, 0, n - 1);
+		answer[arr[i].first] = find(segtree, 1, 0, n - 1, 0, arr[i].first);
+	}
+	for (const auto& elm : answer) cout << elm << endl;
 	
 }
