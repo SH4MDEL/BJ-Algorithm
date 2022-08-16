409bef0a604c97009855147a6aa16bfe646e1214
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2022-08-17 00:31:09 +0900

    solver 1197 - 최소 스패닝 트리
    gold 4
    최소 스패닝 트리를 구현하는 문제.
    크루스칼 알고리즘을 사용하였다. 따라서 유니온 파인드도 덩달아 공부하였다..
    최소 비용순대로 정렬한 후 유니온 파인드에 속하지 않은 트리만을 차례로 포함시키고, 포함시킨 트리의 크기가 v가 되면 루프를 중단하면 된다.

---
+++ Algorithm/Algorithm/1197 - 최소 스패닝 트리.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define inf 987654321 
+#define endl "\n"
+using namespace std;
+
+struct Vertex {
+	int v, e, w;
+};
+int disjoint_set[10101];
+int v, e;
+
+int uf_find(int n) {
+	if (disjoint_set[n] == -1) return n;
+	return disjoint_set[n] = uf_find(disjoint_set[n]);
+}
+
+bool uf_union(int a, int b) {
+	a = uf_find(a);
+	b = uf_find(b);
+	if (a == b) return false;
+	disjoint_set[b] = a;
+	return true;
+}
+
+
+int main()
+{
+	cin >> v >> e;
+	for (int i = 0; i < v; ++i) disjoint_set[i] = -1;
+
+	int a, b, c;
+	vector<Vertex> vertex(e);
+	for (int i = 0; i < e; ++i) {
+		cin >> a >> b >> c;
+		vertex.push_back({ a, b, c });
+	}
+	sort(vertex.begin(), vertex.end(), [](const Vertex& a, const Vertex& b) {
+		return a.w < b.w;
+	});
+	int ans = 0, cnt = 0;
+	for (const auto& elm : vertex) {
+		if (uf_union(elm.v, elm.e)) {
+			ans += elm.w;
+			++cnt;
+		}
+		if (cnt == v) break;
+	}
+	cout << ans << endl;
+}
