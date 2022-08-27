765350607d3ef192c5d8f690d29ea550849e3617
Seungjun Lee <beeutiful@naver.com>
2022-08-27 17:10:35 +0900

    update test.cpp

--- Algorithm/Algorithm/test.cpp
+++ Algorithm/Algorithm/test.cpp
@@ -4,26 +4,16 @@
 #define endl "\n"
 using namespace std;
 
-int n, k, now, maximum, sum;
-int num[100100];
+int n, sum;
 
 int main()
 {
 	std::cin.tie(nullptr);  std::ios::sync_with_stdio(false);
 
-	cin >> n >> k;
-	for (int i = 0; i < n; ++i) {
-		cin >> num[i];
+	while (true) {
+		cin >> n;
+		if (n == -1) break;
+		sum += n;
 	}
-	for (int i = 0; i < k; ++i) {
-		sum += num[i];
-	}
-	maximum = sum;
-	for (int i = k; i < n; ++i) {
-		sum += num[i];
-		sum -= num[now];
-		++now;
-		maximum = max(maximum, sum);
-	}
-	cout << maximum << endl;
+	cout << sum;
 }
