1a7756e47692b3902c608a7e39625e8eb24552ee
Seungjun Lee <beeutiful@naver.com>
2023-09-26 23:29:43 +0900

    11003 - 최솟값 찾기
    platinum 5
    L은 매우 클 수 있다. 따라서 전체 구간을 업데이트 하는 것은 불가능하다.
    L은 일정하다. 항상 배열의 시작에서 값이 빠지고, 배열의 끝에서 값이 들어온다.
    이것이 최소값이어야 한다.
    우선순위 큐를 쓰면 가장 작은 값을 찾을 수 있다. 그러나 배열 시작에서 값을 뺄 수는 없다.
    
    값과 인덱스가 저장된 클래스를 만들고, 우선순위는 값으로 지정한다.
    우선순위 큐에 삽입하고 꺼낸다.
    꺼낸 값이 인덱스에 포함되어 있지 않으면, pop하고 다음 값을 찾는다.

---
+++ Algorithm/Algorithm/11003 - 최솟값 찾기.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321 
+#define endl "\n"
+using namespace std;
+
+int n, l;
+
+struct Data {
+    int num, index;
+};
+struct cmp {
+    bool operator()(const Data& a, const Data& b) {
+        if (a.num == b.num) return a.index > b.index;
+        return a.num > b.num;
+    }
+};
+priority_queue<Data, vector<Data>, cmp> pq;
+
+int main()
+{
+    fastip; sws;
+
+    cin >> n >> l;
+    for (int i = 1; i <= n; ++i) {
+        Data d;
+        d.index = i;
+        cin >> d.num;
+        pq.push(d);
+
+        while (true) {
+            auto now = pq.top();
+            if (now.index < i - l + 1) {
+                pq.pop();
+                continue;
+            }
+            cout << now.num << " ";
+            break;
+        }
+    }
+    //while (!pq.empty()) {
+    //    auto now = pq.top();
+    //    cout << now.index << " " << now.num << endl;
+    //    pq.pop();
+    //}
+}
