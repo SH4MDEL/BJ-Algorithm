b2e9a6a156da4ef8588d1f9c5f0a9d25b3d1bdda
Seungjun Lee <beeutiful@naver.com>
2023-10-07 15:22:40 +0900

    solve 2217 - 로프
    silver 4
    간단한 정렬 문제

---
+++ Algorithm/Algorithm/2217 - 로프.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321 
+#define endl "\n"
+using namespace std;
+
+int n;
+vector<int> rope;
+
+int main()
+{
+	fastip; sws;
+
+	cin >> n;
+	rope.resize(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> rope[i];
+	}
+	sort(rope.begin(), rope.end(), [](const int& a, const int& b) {
+		return a > b;
+		});
+	int maxNum = -inf;
+	for (int i = 0; i < n; ++i) {
+		maxNum = max(rope[i] * (i + 1), maxNum);
+	}
+	cout << maxNum;
+}
