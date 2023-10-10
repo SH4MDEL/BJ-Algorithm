c3dfb350ab0ec432de86e884e1ef3e8f48345cf7
Seungjun Lee <beeutiful@naver.com>
2023-10-11 00:36:51 +0900

    solve 1202 - 보석 도둑
    gold 2
    정렬과 우선순위 큐 활용 문제.
    보석은 가치와 무게를 갖는데, 가장 가까운 보석으로 정렬한다. 가방 역시 가장 가벼운 가방으로 정렬한다.
    맨 앞의 가방에 들어갈 수 있는 보석만 우선순위 큐에 넣는다. 우선순위 큐는 가치로 정렬된다.
    이제 가장 큰 우선순위를 갖는 보석을 맨 앞의 가방에 넣으면 된다. 이를 모든 가방에 대해 반복한다.

---
+++ Algorithm/Algorithm/1202 - 보석 도둑.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321 
+#define endl "\n"
+using namespace std;
+
+int n, k;
+struct jewel {
+    int weight;
+    int value;
+
+    bool operator<(const jewel& rhs) {
+        return weight < rhs.weight;
+    }
+};
+struct cmp {
+    bool operator()(const jewel& a, const jewel& b) {
+        return a.value < b.value;
+    }
+};
+vector<jewel> v;
+vector<int> bag;
+priority_queue<jewel, vector<jewel>, cmp> pq;
+long long ans;
+
+int main()
+{
+    fastip; sws;
+
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i) {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({ a, b });
+    }
+    for (int i = 0; i < k; ++i) {
+        int a;
+        cin >> a;
+        bag.push_back(a);
+    }
+    sort(v.begin(), v.end());
+    sort(bag.begin(), bag.end());
+
+    auto itr = v.begin();
+    for (const auto& size : bag) {
+        while (itr != v.end() && (*itr).weight <= size) {
+            pq.push(*itr);
+            ++itr;
+        }
+        if (!pq.empty()) {
+            ans += pq.top().value;
+            pq.pop();
+        }
+    }
+    cout << ans;
+}
