d533b7faf7a58c75982ecafbd1d86a43eceb0116
Seungjun Lee <beeutiful@naver.com>
2023-10-19 14:42:51 +0900

    solve 1368 - 물대기
    gold 2
    최소 스패닝 트리 응용 문제.
    새로 우물을 파는 작업을, 0번 논과 연결한다고 생각할 수 있다.
    이 경우 평범한 최소 스패닝 트리 문제가 된다.

--- Algorithm/Algorithm/1368 - 물대기.cpp
+++ Algorithm/Algorithm/1368 - 물대기.cpp
@@ -16,47 +16,47 @@ struct Node {
 };
 priority_queue<Node> pq;
 int n;
-int disjoint_set[1010];
+int disjoint_set[310];
 
-int uf_find(int n)
-{
-	if (disjoint_set[n] == -1) return n;	// 내가 부모이다.
-	return uf_find(disjoint_set[n]);	// 부모를 찾는다.
+int uf_find(int n) {
+	if (disjoint_set[n] == -1) return n;
+	return uf_find(disjoint_set[n]);
 }
 
-bool uf_union(int a, int b)
-{
-	a = uf_find(a);		// 각자의 부모를 찾는다.
+bool uf_union(int a, int b) {
+	a = uf_find(a);
 	b = uf_find(b);
-	if (a == b) return false;	// 실패한다면 이미 연결되어 있다는 뜻
+	if (a == b) return false;
 	disjoint_set[b] = a;
 	return true;
 }
 
 int main() {
 	fastip; sws;
-
 	cin >> n;
 	fill(disjoint_set, disjoint_set + n + 1, -1);
+	for (int i = 1; i <= n; ++i) {
+		int a;
+		cin >> a;
+		pq.push({ 0, i, a });
+	}
 	for (int i = 1; i <= n; ++i) {
 		for (int j = 1; j <= n; ++j) {
 			int a;
 			cin >> a;
-			if (i != j) {
-				pq.push({ i, j, a });
-			}
+			if (i != j) pq.push({ i, j, a });
 		}
 	}
-	long long ans = 0, count = 0;
-	while (count != n - 1) {
+	int count = 0, cost = 0;
+	while (count != n) {
 		int s = pq.top().s;
 		int e = pq.top().e;
 		int v = pq.top().v;
 		pq.pop();
 		if (uf_union(s, e)) {
 			++count;
-			ans += v;
+			cost += v;
 		}
 	}
-	cout << ans;
+	cout << cost;
 }
