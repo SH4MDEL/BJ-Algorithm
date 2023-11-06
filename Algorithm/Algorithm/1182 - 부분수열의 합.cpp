14c304e2e2baf88a8e09561d95f1b596c84ccd8c
Seungjun Lee <beeutiful@naver.com>
2023-11-06 23:27:51 +0900

    solve 1182 - 부분수열의 합
    silver 2
    백트레킹을 통한 브루트포스를 통해 가능한 조합을 전부 구해보는 문제.

---
+++ Algorithm/Algorithm/1182 - 부분수열의 합.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321 
+#define endl "\n"
+using namespace std;
+
+int n, s;
+vector<int> v;
+
+int search(int now, int c)
+{
+	if (c == n) return 0;
+	
+	int num = now + v[c];
+	if (num == s) {
+		cout << now << endl;
+		return search(now, c + 1) + search(num, c + 1) + 1;
+	}
+	return search(now, c + 1) + search(num, c + 1);
+}
+
+int main()
+{
+	sws; fastip;
+
+	cin >> n >> s;
+	v.resize(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	cout << search(0, 0);
+	
+}
