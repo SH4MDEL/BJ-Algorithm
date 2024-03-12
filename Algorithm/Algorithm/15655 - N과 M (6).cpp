05e2880c7b76cc0925e0087a816f4684657a0291
Seungjun Lee <beeutiful@naver.com>
2024-03-12 15:43:07 +0900

    solve 15655 - N과 M (6)
    silver 3
    간단한 재귀 문제

---
+++ Algorithm/Algorithm/15655 - N과 M (6).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321 
+#define endl "\n"
+using namespace std;
+
+unordered_set<string> s;
+int num[8];
+int n, m;
+
+void solve(string str, int depth, int index)
+{
+	if (depth == m) {
+		cout << str << endl;
+		return;
+	}
+
+	for (int i = index; i < n; ++i) {
+		solve(str + to_string(num[i]) + " ", depth + 1, i + 1);
+	}
+}
+
+int main()
+{
+	fastip; sws;
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
+	solve("", 0, 0);
+}
