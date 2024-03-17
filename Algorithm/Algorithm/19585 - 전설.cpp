8bf543e7a9ccf60ba76073145ffa9011686b0818
Seungjun Lee <beeutiful@naver.com>
2024-03-17 14:42:39 +0900

    solve 19585 - 전설
    platinum 3
    해쉬맵 대신 map 사용으로 메모리 절약.
    트리 두 개를 구성하는 것이 아니라 트리 하나와 해쉬를 활용한다.

--- Algorithm/Algorithm/19585 - 전설.cpp
+++ Algorithm/Algorithm/19585 - 전설.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
-#include <vector>
+#include <map>
+#include <memory>
 #define fastip std::cin.tie(nullptr)
 #define sws std::ios::sync_with_stdio(false)
 #define inf 987654321 
@@ -9,69 +10,48 @@
 #define ll long long
 using namespace std;
 
-struct Node;
-Node* croot;
-
-#define mod 1'000'000'007
-#define prime 29
-size_t hashfunc(const string& s) {
-	size_t hash_value = 0;
-	size_t p_pow = 1;
-	for (const auto& c : s) {
-		hash_value = (hash_value + (c - 'a' + 1) * p_pow) % mod;
-		p_pow = (p_pow * prime) % mod;
-	}
-	return hash_value;
-}
-
-unordered_set<string, hash<string>> name;
-
+unordered_set<string> name;
+string s;
 struct Node {
-	Node() { children.resize(26); }
 	bool finished;
 
-	vector<Node*> children;
+	map<char, shared_ptr<Node>> children;
 
-	void insert(const string& s, int depth) {
+	void insert(int depth) {
 		if (depth == s.length()) {
 			finished = true;
 			return;
 		}
-		if (!children[s[depth] - 'a']) children[s[depth] - 'a'] = new Node;
-		children[s[depth] - 'a']->insert(s, depth + 1);
+		if (!children.count(s[depth])) children[s[depth]] = make_shared<Node>();
+		children[s[depth]]->insert(depth + 1);
 	}
 
-	string search_color(const string& s, int depth) {
+	string search_color(int depth) {
 		if (depth == s.length()) return "No";
-		if (finished) {
-			if (name.count(s.substr(depth, s.length()))) return "Yes";
-		}
-		if (!children[s[depth] - 'a']) return "No";
-		return children[s[depth] - 'a']->search_color(s, depth + 1);
+		if (finished && name.count(s.substr(depth, s.length()))) return "Yes";
+		if (!children.count(s[depth])) return "No";
+		return children[s[depth]]->search_color(depth + 1);
 	}
 };
-
+Node root;
 int c, n, q;
 
 int main()
 {
 	fastip; sws;
-	croot = new Node;
+	root;
 	cin >> c >> n;
 	for (int i = 0; i < c; ++i) {
-		string s;
 		cin >> s;
-		croot->insert(s, 0);
+		root.insert(0);
 	}
 	for (int i = 0; i < n; ++i) {
-		string s;
 		cin >> s;
 		name.insert(s);
 	}
 	cin >> q;
 	for (int i = 0; i < q; ++i) {
-		string s;
 		cin >> s;
-		cout << croot->search_color(s, 0) << endl;
+		cout << root.search_color(0) << endl;
 	}
 }
