aa397aeb31f7da54a8ffdef4f0cd43ca61b7c8fd
Seungjun Lee <beeutiful@naver.com>
2024-04-06 13:31:42 +0900

    solve 13909 - 창문 닫기
    silver 5
    어떤 창문이 짝수 번 선택되었으면 닫혀 있고, 홀수 번 선택되었으면 열려 있는 상태이다.
    이 창문의 약수의 개수가 홀수개이면 열려 있고, 짝수개이면 닫혀 있는 상태일 것이다.
    예를 들어 6은 1 2 3 6이 건드리게 되므로 닫혀 있는 상태가 된다.
    그리고 이후로 7, 8로 늘어나더라도 6을 건드릴 수 있는 사람이 늘어나지 않으므로 6의 상태는 닫힌 채로 고정이다.
    그러면 약수의 개수가 홀수인 경우를 어떻게 판정하느냐, 정수인 sqrt(n)이 존재하면 약수의 개수가 홀수이다.
    즉
    1 * 1 = 1번째 창문이 열려 있는 상태가 되고
    2 * 2 = 4번째 창문이 열려 있는 상태가 되고
    3 * 3 = 9번째 창문이 열려 있는 상태가 된다.
    따라서 n이 9라면 답은 sprt(n)인 3이 된다.
    n이 10이라면? static_cast<int>(sqrt(n))가 답이다.

---
+++ Algorithm/Algorithm/13909 - 창문 닫기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321 
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+	fastip; sws;
+	int n;
+	cin >> n;
+	cout << static_cast<int>(sqrt(n));
+}
