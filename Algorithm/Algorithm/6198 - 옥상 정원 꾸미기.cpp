1d6aefc550174b8f4eefa715fa98c5a807e315f4
Seungjun Lee <beeutiful@naver.com>
2024-05-22 22:23:41 +0900

    solve 6198 - 옥상 정원 꾸미기
    gold 5
    스택 활용 문제.

---
+++ Algorithm/Algorithm/6198 - 옥상 정원 꾸미기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 10987654321
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+int n, num, ans;
+stack<int> s;
+
+int main()
+{
+	fastip; sws;
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> num;
+		while (!s.empty() && s.top() <= num) {
+			s.pop();
+		}
+		ans += s.size();
+		s.push(num);
+	}
+	cout << ans;
+}
