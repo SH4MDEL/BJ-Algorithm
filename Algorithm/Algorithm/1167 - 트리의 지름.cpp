e79381e709fb0ed88fa93c3d1de49b8e7092f5f1
Seungjun Lee <beeutiful@naver.com>
2024-05-22 16:07:38 +0900

    solve 1167 - 트리의 지름
    gold 2
    문제를 단순화시켜 어떤 노드의 자식 노드로 향하는 가장 긴 경로 l을 구한다고 생각해 보자. 이는 간단한 dfs다.
    어떤 노드를 루트 노드로 잡았을 때, 다음 두 가지 경우를 고려해보자.
    1. 나를 종점으로 하는 가장 긴 경로
    2. 트리의 지름
    1번의 경우 l이 답이다.
    2번의 경우 두 가지를 고려해야 한다.
    1. 트리의 지름이 나를 포함함.
    2. 트리의 지름이 내 자식 단에서 결정됨.
    1번은 가장 긴 l과 두 번째로 긴 l의 합이다.
    2번은 자식으로부터 재귀적으로 구한 2의 최댓값이다.
    1번과 2번의 최댓값이 트리의 지름이 된다.

---
+++ Algorithm/Algorithm/1167 - 트리의 지름.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#define fastip std::cin.tie(nullptr)
+#define sws std::ios::sync_with_stdio(false)
+#define inf 987654321
+#define mod 9901
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+int v;
+vector<tuple<int, int>> children[100100];
+
+// 내 자식들 중 가장 먼 두놈 or 나를 종점으로 하는 가장 먼 놈까지 거리.
+tuple<int, int> search(int me, int parent)
+{
+	if (parent != -1 && children[me].size() == 1) {
+		return {0, 0};
+	}
+
+	int maxround = 0, maxway1 = 0, maxway2 = 0;
+	for (const auto& [child, dist] : children[me]) {
+		if (child == parent) continue;
+		auto [round, way] = search(child, me);
+		way += dist;
+		maxround = max(maxround, round);
+		if (way >= maxway1) {
+			maxway2 = maxway1;
+			maxway1 = way;
+		}
+		else if (way > maxway2) {
+			maxway2 = way;
+		}
+	}
+
+	return { max(maxround, maxway1 + maxway2), maxway1 };
+}
+
+int main()
+{
+	fastip; sws;
+
+	cin >> v;
+	for (int i = 0; i < v; ++i) {
+		int from, to, dist; 
+		cin >> from >> to;
+		while (to != -1) {
+			cin >> dist;
+			children[from].push_back({to, dist});
+			cin >> to;
+		}
+	}
+	auto [ans, _] = search(1, -1);
+	cout << ans;
+}
